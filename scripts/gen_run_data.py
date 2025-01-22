2e124bda06bd8e9e9441d2afd36dc3039cebf75c
Matt Kramer <mjkramer@users.noreply.github.com>
2025-01-21 18:57:45 -0800

    Make gen_run_data.py support both 2x2 and FSD

--- scripts/gen_run_data.py
+++ scripts/gen_run_data.py
@@ -6,8 +6,15 @@ import sqlite3
 
 HEADER = 'e_field charge_filename light_filename charge_thresholds light_samples'
 
-# In the FSD, at a cathode voltage of (-)23.4 kV, drift field is 500 V/cm
-KV_TO_VPERCM_FSD = 500 / 23.4
+KV_TO_VPERCM = {
+    '2x2': 500 / 26.2,
+    'fsd': 500 / 23.4,
+}
+
+HV_EXPRESSION = {
+    '2x2': '-s.Mean_Spellman_set_voltage_kV', # note negative sign!
+    'fsd': 's.HV_kV',
+}
 
 DEFAULT_CHARGE_THRESHOLDS = 'high'
 DEFAULT_LIGHT_SAMPLES = 256
@@ -17,6 +24,8 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('-d', '--run-db', required=True)
     ap.add_argument('-o', '--output', required=True)
+    ap.add_argument('-c', '--config', required=True,
+                    choices=['2x2', 'fsd'])
     args = ap.parse_args()
 
     outf = open(args.output, 'w')
@@ -24,19 +33,21 @@ def main():
 
     conn = sqlite3.connect(args.run_db)
 
-    q = 'SELECT c.filename, l.filename, s.HV_kV \
-         FROM (All_global_subruns a JOIN SC_beam_summary s \
-               ON a.global_subrun = s.global_subrun) \
-         JOIN CRS_summary c \
-              ON c.run = a.crs_run AND c.subrun = a.crs_subrun \
-         JOIN LRS_summary l \
-              ON l.run = a.lrs_run AND l.subrun = a.lrs_subrun'
+    hv_expr = HV_EXPRESSION[args.config]
+
+    q = f'SELECT c.filename, l.filename, {hv_expr} \
+          FROM (All_global_subruns a JOIN SC_beam_summary s \
+                ON a.global_subrun = s.global_subrun) \
+          JOIN CRS_summary c \
+               ON c.run = a.crs_run AND c.subrun = a.crs_subrun \
+          JOIN LRS_summary l \
+               ON l.run = a.lrs_run AND l.subrun = a.lrs_subrun'
 
     for row in conn.execute(q):
         charge_filename, light_filename, kV = row
         if charge_filename.startswith('binary-'):
             charge_filename = charge_filename.replace('binary-', 'packet-', 1)
-        e_field = -kV * KV_TO_VPERCM_FSD
+        e_field = -kV * KV_TO_VPERCM[args.config]
         charge_thresholds = DEFAULT_CHARGE_THRESHOLDS
         light_samples = DEFAULT_LIGHT_SAMPLES
 
