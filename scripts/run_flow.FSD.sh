a958fc9b5f0492f9ce6b7f5cb0ff7950484dcb6c
Matt Kramer <mjkramer@users.noreply.github.com>
2025-03-11 16:10:23 -0700

    Update FSD yamls

--- scripts/run_flow.FSD.sh
+++ scripts/run_flow.FSD.sh
@@ -3,18 +3,18 @@
 set -o errexit
 
 # charge workflows
-workflow_charge_event_build='yamls/fsd_flow/workflows/charge/charge_event_building.yaml'
-workflow_charge_event_reco='yamls/fsd_flow/workflows/charge/charge_event_reconstruction.yaml'
-workflow_comb_reco='yamls/fsd_flow/workflows/combined/combined_reconstruction.yaml'
-workflow_charge_hit_reco_prompt='yamls/fsd_flow/workflows/charge/prompt_calibration.yaml'
+workflow_charge_event_build='yamls/fsd_flow/workflows/charge/charge_event_building_data.yaml'
+workflow_charge_event_reco='yamls/fsd_flow/workflows/charge/charge_event_reconstruction_data.yaml'
+workflow_comb_reco='yamls/fsd_flow/workflows/combined/combined_reconstruction_data.yaml'
+workflow_charge_hit_reco_prompt='yamls/fsd_flow/workflows/charge/prompt_calibration_data.yaml'
 # workflow_charge_hit_reco_final='yamls/fsd_flow/workflows/charge/final_calibration.yaml'
 
 # light workflows
 workflow_light_event_build='yamls/fsd_flow/workflows/light/light_event_building_mpd.yaml'
-workflow_light_event_reco='yamls/fsd_flow/workflows/light/light_event_reconstruction.yaml'
+workflow_light_event_reco='yamls/fsd_flow/workflows/light/light_event_reconstruction_data.yaml'
 
 # charge-light trigger matching
-workflow_charge_light_match='yamls/fsd_flow/workflows/charge/charge_light_assoc.yaml'
+workflow_charge_light_match='yamls/fsd_flow/workflows/charge/charge_light_assoc_data.yaml'
 
 outf=$(realpath "$1"); shift
 chargef=$(realpath "$1"); shift
