7795fa2d80f3bfd4e1d9e910b92ed74d44f817df
Matt Kramer <mjkramer@users.noreply.github.com>
2025-04-02 16:58:37 -0700

    Re-disable final calib
    (hit merger)

--- scripts/run_flow.FSD.sh
+++ scripts/run_flow.FSD.sh
@@ -5,7 +5,7 @@ workflow_charge_event_build='yamls/fsd_flow/workflows/charge/charge_event_buildi
 workflow_charge_event_reco='yamls/fsd_flow/workflows/charge/charge_event_reconstruction_data.yaml'
 workflow_comb_reco='yamls/fsd_flow/workflows/combined/combined_reconstruction_data.yaml'
 workflow_charge_hit_reco_prompt='yamls/fsd_flow/workflows/charge/prompt_calibration_data.yaml'
-workflow_charge_hit_reco_final='yamls/fsd_flow/workflows/charge/final_calibration_data.yaml'
+# workflow_charge_hit_reco_final='yamls/fsd_flow/workflows/charge/final_calibration_data.yaml'
 
 # light workflows
 workflow_light_event_build='yamls/fsd_flow/workflows/light/light_event_building_mpd.yaml'
