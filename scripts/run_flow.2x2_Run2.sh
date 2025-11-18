962ba3bc5ebd2d682e1e949994ddff314b5ff77e
Matt Kramer <mjkramer@users.noreply.github.com>
2025-11-17 22:20:58 -0800

    Add run_flow.2x2_Run2.sh

---
+++ scripts/run_flow.2x2_Run2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# charge workflows
+workflow_charge_event_build='yamls/proto_nd_flow/workflows/charge/charge_event_building_data_Run2.yaml'
+workflow_charge_event_reco='yamls/proto_nd_flow/workflows/charge/charge_event_reconstruction_data_Run2.yaml'
+workflow_comb_reco='yamls/proto_nd_flow/workflows/combined/combined_reconstruction_data.yaml'
+workflow_charge_hit_reco_prompt='yamls/proto_nd_flow/workflows/charge/prompt_calibration_data_Run2.yaml'
+workflow_charge_hit_reco_final='yamls/proto_nd_flow/workflows/charge/final_calibration_data_Run2.yaml'
+
+# light workflows
+workflow_light_event_build='yamls/proto_nd_flow/workflows/light/light_event_building_mpd_Run2.yaml'
+workflow_light_event_reco='yamls/proto_nd_flow/workflows/light/light_event_reconstruction_data.yaml'
+
+# charge-light trigger matching
+workflow_charge_light_match='yamls/proto_nd_flow/workflows/charge/charge_light_assoc_data.yaml'
+
+source "$(dirname "$0")/really_run_flow.inc.sh"
