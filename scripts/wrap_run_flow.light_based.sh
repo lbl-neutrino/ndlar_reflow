d72c57fd86ce1cdc16f9b98582f0c81a4fd193d8
Matt Kramer <mjkramer@users.noreply.github.com>
2025-11-22 13:57:54 -0800

    Add light-based wrapper

---
+++ scripts/wrap_run_flow.light_based.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+lightf=$ND_PRODUCTION_LIGHT_FILE
+# split $ND_PRODUCTION_CHARGE_FILES into an array $chargefs
+read -r -a chargefs <<< "$ND_PRODUCTION_CHARGE_FILES"
+
+light_relpath=$(echo "$lightf" | sed "s|^$ND_PRODUCTION_INDIR_BASE/||")
+light_reldir=$(dirname "$light_relpath")
+
+outname=$(basename "$light_relpath" .data)$ND_PRODUCTION_FILE_TAG
+
+# HACK for Run 2:
+outname=$(echo "$outname" | sed 's/mpd_run_data/mpd_run_run2data')
+
+outf=$ND_PRODUCTION_OUTDIR_BASE/$light_reldir/$outname.FLOW.hdf5
+logf=$ND_PRODUCTION_LOGDIR_BASE/$light_reldir/$outname.log
+
+inner_script=scripts/really_run_flow.light_based.inc.sh
+outer_script=scripts/run_flow.$ND_PRODUCTION_REFLOW_VARIANT.sh
+if [[ ! -e "$outer_script" ]]; then
+    echo "Unable to find $outer_script" 2>&1
+    exit 1
+fi
+
+mkdir -p "$(dirname "$outf")" "$(dirname "$logf")"
+
+/usr/bin/time -f "%P %M %E" "$outer_script" "$inner_script" \
+    "$outf" "$lightf" "${chargefs[@]}" 2>&1 | tee -a "$logf"
